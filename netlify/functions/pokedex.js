528096b92a799d3d3ea0d1d71f4fa28a38fe3d8d
witcradg <witcradg@gmail.com>
2023-03-27 13:07:10 -0700

    Netlify server tutorial pokedex api

---
+++ netlify/functions/pokedex.js
@@ -0,0 +1,12 @@
+exports.handler = async function () {
+    const POKE_API = 'https://pokeapi.co/api/v2/pokemon/charmander'
+
+    const response = await fetch (POKE_API)
+
+    const data = await response.json()
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data)
+    }
+}
